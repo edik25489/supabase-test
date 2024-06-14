fd2fd51152d9d47824487d1d9a0f1edf4c8c0493
Eduard Yakovlev <edik25mas@yandex.ru>
2024-06-14 22:00:02 +0300

    init

---
+++ app.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { createClient } from '@supabase/supabase-js'
+const supabase = createClient('https://gwzpmebibriyvqjtlaan.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3enBtZWJpYnJpeXZxanRsYWFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE0NzIzNDYsImV4cCI6MjAyNzA0ODM0Nn0.3LXCdJZMckFZqpbYzy4pZmJe82xBNh_neKyimnlea48')
+const countries = ref([])
+
+async function getCountries() {
+  const { data } = await supabase.from('images').select()
+  countries.value = data
+}
+
+onMounted(() => {
+  getCountries()
+})
+</script>
+
+<template>
+    <p v-for="country in countries" :key="country.id">
+      <img :src=country.src :alt=country.alt style="height: 200px; width: 200px; object-fit: cover;">
+    </p>
+</template>
